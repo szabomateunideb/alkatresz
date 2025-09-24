e1911897682c444e901b0eddc7bc46d20714e27a
Máté <szabo.mate@inf.unideb.hu>
2025-09-24 15:27:12 +0200

    init

---
+++ src/main/java/hu/unideb/inf/alkatresz/AlkatreszApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.alkatresz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkatreszApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlkatreszApplication.class, args);
+	}
+
+}
