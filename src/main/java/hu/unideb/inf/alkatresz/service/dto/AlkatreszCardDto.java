9a3ffcfb003767d29111d7f03dd7c616261b7829
Máté <szabo.mate@inf.unideb.hu>
2025-10-08 14:22:51 +0200

    AlkatreszCardDto

---
+++ src/main/java/hu/unideb/inf/alkatresz/service/dto/AlkatreszCardDto.java
@@ -0,0 +1,12 @@
+package hu.unideb.inf.alkatresz.service.dto;
+
+import jakarta.persistence.Column;
+
+public class AlkatreszCardDto {
+
+    private Long id;
+    private String nev;
+    private String tipus;
+    private String meret;
+    private String gyarto;
+}
